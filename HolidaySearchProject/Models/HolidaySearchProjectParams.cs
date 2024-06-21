b17ff5da2fd9eb9bed823465916f4d57d626f657
Gbenga Obasa <gbenga@theo-tech.co.uk>
2024-06-21 22:37:32 +0100

    Create Holiday Search Parameter Model
    * Created a model that give sthe ability for inputting search parameters for a holiday.

---
+++ HolidaySearchProject/Models/HolidaySearchProjectParams.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HolidaySearchProject.Models
+{
+    public class HolidaySearchProjectParams
+    {
+        public string DepartingFrom { get; set; }
+        public string TravellingTo { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
